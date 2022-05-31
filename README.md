88649bd7900b77e39e981e6d5daa6bfb0a137d1c
Charly Aguirre <charagmz@gmail.com>
2022-05-31 03:56:28 -0500

    Init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Notas:
+Rest server
---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT;
+
+
+app.get('/', (req, res) => {
+    res.send('hola mundo')
+});
+
+
+app.listen(port);
+
+
+console.log('Servidor escuchando en puerto', port);
+
